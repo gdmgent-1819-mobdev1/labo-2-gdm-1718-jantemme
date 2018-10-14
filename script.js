db64b2a1887eac4a5532857977285c454d86d211
Jan Temmerman <jantemme@student.arteveldehs.be>
2018-10-15 00:09:36 +0200

    map added WIP

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let persons = [];
 let likes = [];
 let dislikes = [];
 let samePerson = false;
+let lat1, lon1, lat2 = 0, lon2 = 0;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFudGVtbWUiLCJhIjoiY2puMzUxdnl1MzZiNDNxbzhhMjZuZW8ydiJ9.VnvzvyT9kZRkmsgM_gCtdw';
+        var map = new mapboxgl.Map({
+            container: 'pastPeople/map', // HTML container id
+            style: 'mapbox://styles/mapbox/streets-v9', // style URL
+            zoom: 7
+        });
 
 function NewPerson(person, storageCounter) {
     let thisPerson = {};
@@ -35,6 +43,7 @@ function NewPerson(person, storageCounter) {
     }
 
     function showProfile(){
+        document.getElementById('infoholder').innerHTML = "";
         persons.forEach(person => {
             if(person.userID == people.results[storageCounter].login.uuid)
                 samePerson = true;
@@ -44,11 +53,11 @@ function NewPerson(person, storageCounter) {
 
         if(!samePerson)
         {
-            document.getElementById('infoholder').innerHTML = "<h2>"+ people.results[storageCounter].name.first + " " + people.results[storageCounter].name.last + ", " + people.results[storageCounter].dob.age + "</h2> </br> <h2>" + people.results[storageCounter].location.city + "</h2>";
+            document.getElementById('infoholder').innerHTML = "<h2>"+ people.results[storageCounter].name.first + " " + people.results[storageCounter].name.last + ", " + people.results[storageCounter].dob.age + "</h2><h2>" + people.results[storageCounter].location.city + "</h2>";
             document.getElementById('imageholder').innerHTML = "<img src='" + people.results[storageCounter].picture.large + "' alt='profilepicture'>";
+            getDistance();
             persons.push(new NewPerson(people, storageCounter));
         }
-        console.log(samePerson);
     }
 
     function dislike(){
@@ -118,9 +127,9 @@ function NewPerson(person, storageCounter) {
     }
 
     function showDislikes(){
-        document.getElementById('pastPeople').innerHTML = "<span id='close' class='close'>&times;</span><h3>Dislikes</h3><h3>click name to like</h3>";
+        document.getElementById('pastPeople/map').innerHTML = "<span id='close' class='close'>&times;</span><h3>Dislikes</h3><h3>click name to like</h3>";
         dislikes.forEach(person => {
-            document.getElementById('pastPeople').innerHTML += "<h4>" + person.firstName + " " + person.lastName + "</h4>";
+            document.getElementById('pastPeople/map').innerHTML += "<h4>" + person.firstName + " " + person.lastName + "</h4>";
         });
         document.getElementById('modal').style.display = "block";
         let names = document.getElementsByTagName('h4');
@@ -141,9 +150,9 @@ function NewPerson(person, storageCounter) {
     }
 
     function showLikes(){
-        document.getElementById('pastPeople').innerHTML = "<span id='close' class='close'>&times;</span><h3>Likes</h3><h3>click name to dislike</h3>";
+        document.getElementById('pastPeople/map').innerHTML = "<span id='close' class='close'>&times;</span><h3>Likes</h3><h3>click name to dislike</h3>";
         likes.forEach(person => {
-            document.getElementById('pastPeople').innerHTML += "<h4>" + person.firstName + " " + person.lastName + "</h4>";
+            document.getElementById('pastPeople/map').innerHTML += "<h4>" + person.firstName + " " + person.lastName + "</h4>";
         });
         document.getElementById('modal').style.display = "block";
         let names = document.getElementsByTagName('h4');
@@ -163,6 +172,59 @@ function NewPerson(person, storageCounter) {
         }
     }
 
+    function toRadians(degrees){
+    var pi = Math.PI;
+    return degrees * (pi/180);
+    }
+
+    function getDistance(){
+        lat2 = people.results[storageCounter].location.coordinates.latitude;
+        lon2 = people.results[storageCounter].location.coordinates.longitude;
+
+        let R = 6371e3; // metres
+        let φ1 = toRadians(lat1);
+        let φ2 = toRadians(lat2);
+        let Δφ = toRadians(lat2-lat1);
+        let Δλ = toRadians(lon2-lon1);
+
+        let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        let d = R * c;
+
+        document.getElementById('infoholder').innerHTML += "<h2 id='distance' class='distance'>" + Math.round(d) + "km away</h2>";
+        document.getElementById('distance').addEventListener("click", showMap);
+    }
+
+    function getLocation(){
+        if("geolocation" in navigator){
+            navigator.geolocation.getCurrentPosition(function(position) {
+                lat1 = position.coords.latitude;
+                lon1 = position.coords.longitude;
+            });
+        }
+        else{
+            /* geolocation IS NOT available */
+        }
+    }
+
+    function showMap(){
+        document.getElementById('modal').style.display = "block";
+        
+        map.setCenter([lon2, lat2]);
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        console.log('skrr');
+    }
+
+
+    getLocation();
     loadNew();
     showProfile();
 
@@ -170,3 +232,4 @@ function NewPerson(person, storageCounter) {
     document.getElementById('button--like').addEventListener("click", like);
     document.getElementById('button--dislikes').addEventListener("click", showDislikes);
     document.getElementById('button--likes').addEventListener("click", showLikes);
+    
